0b5b82cc87908e4b9002653aefc162eef6974b24
abkolan <ab.kolan@gmail.com>
2022-11-27 01:26:50 +0000

    Added Envs and Samples

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
-import add from "./math/add";
 import dotenv from 'dotenv-safe';
+import { cwd } from 'process';
+const { CosmosClient } = require("@azure/cosmos");
 
 dotenv.config();
 
-console.log(add(1,5));
-console.log(`My name is: ${process.env.MY_NAME}`);
+const key = process.env.COSMOS_KEY;
+const endpoint = process.env.COSMOS_ENDPOINT;
+const databaseId = process.env.COSMOS_DATABASE || "<cosmos database>";
+
+const client = new CosmosClient({ endpoint, key });
+
+async function main() {
+    console.log("************ START ************");
+    
+   // await test();
+   //await listDatabases();
+   await listContainers();
+   console.log("************  END  ************");
+
+}
+
+async function listDatabases() {
+    const { resources: databases } = await client.databases.readAll().fetchAll();
+    console.log(databases);
+}
+
+async function listContainers() {
+    console.log("************ listContainers ************");
+    const { resources: containers } = await client.database("adventure-works").containers.readAll().fetchAll();
+    for (const container of containers) {
+        console.log(container);
+    }
+}
+
+async function test() {
+    console.log(`key: ${key}`);
+    console.log(`endpoint: ${endpoint}`);
+}
+
+main().catch((error) => {
+    console.error(error);
+});
+
